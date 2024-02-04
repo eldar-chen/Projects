119fc8359bbb1171a82d1163960edbf320dd588b
Echen <156080256+eldar-chen@users.noreply.github.com>
2024-02-04 19:19:35 +0800

    Update_Treasure_hunt.py

--- Treasure_hunt.py
+++ Treasure_hunt.py
@@ -18,23 +18,3 @@ map[number][index] = 'X'
 
 
 print(f"{line1}\n{line2}\n{line3}")
-
-
-# if position == 'A1':
-#   line1[0] = 'X'
-# elif position == 'A2':
-#   line2[0] = 'X'
-# elif position == 'A3':
-#   line3[0] = 'X'
-# elif position == 'B1':
-#   line1[1] == 'X'
-# elif position == 'B2':
-#   line2[1] = 'X'
-# elif position == 'B3':
-#   line3[1] = 'X'
-# elif position == 'C1':
-#   line1[2] = 'X'
-# elif position == 'C2':
-#   line2[2] = 'X'
-# elif position == 'C3':
-#   line3[2] = 'X'
